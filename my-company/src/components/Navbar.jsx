3f125cce6ac6ec515a0a2b31691910e364c778b6
Athanas Muinde <athanasmwandi5@gmail.com>
2025-09-08 01:12:20 +0300

    fixed error

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,14 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: 'navy', padding: '10px', textAlign: 'center' }}>
+    <nav
+      style={{
+        backgroundColor: 'navy',
+        padding: '10px',
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
       <Link to="/" style={{ color: 'white', margin: '0 15px' }}>Home</Link>
       <Link to="/about" style={{ color: 'white', margin: '0 15px' }}>About</Link>
       <Link to="/services" style={{ color: 'white', margin: '0 15px' }}>Services</Link>
