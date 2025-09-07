42b04beb1c03835f4c15479d4cf45ac6aa9356d5
Athanas Muinde <athanasmwandi5@gmail.com>
2025-09-07 23:50:16 +0300

    initial commit

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+         <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+      <p style={{ margin: 0 }}>© 2023 City Lovers</p>
+    </footer>
+    )
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+function MainContent() {
+    return (
+    <main style={{ padding: '20px', backgroundColor: '#f0f8ff', textAlign: 'center' }}>
+      <p style={{ fontSize: '18px', color: '#333', margin: '10px 0' }}>
+        I love to visit <span style={{ color: 'darkred', fontWeight: 'bold' }}>New York</span>,{" "}
+        <span style={{ color: 'darkgreen', fontWeight: 'bold' }}>Paris</span>, and{" "}
+        <span style={{ color: 'darkblue', fontWeight: 'bold' }}>Tokyo</span>.
+      </p>
+    </main>
+    )
+}
+
+export default MainContent;
