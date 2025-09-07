d03acbe4b70f4cd650f2c08a53e41f9c31521d72
Athanas Muinde <athanasmwandi5@gmail.com>
2025-09-07 23:55:20 +0300

    fixed error

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,10 +1,8 @@
 function MainContent() {
     return (
-    <main style={{ padding: '20px', backgroundColor: '#f0f8ff', textAlign: 'center' }}>
+         <main style={{ padding: '20px', backgroundColor: '#f0f8ff', textAlign: 'center' }}>
       <p style={{ fontSize: '18px', color: '#333', margin: '10px 0' }}>
-        I love to visit <span style={{ color: 'darkred', fontWeight: 'bold' }}>New York</span>,{" "}
-        <span style={{ color: 'darkgreen', fontWeight: 'bold' }}>Paris</span>, and{" "}
-        <span style={{ color: 'darkblue', fontWeight: 'bold' }}>Tokyo</span>.
+        I love to visit New York, Paris, and Tokyo.
       </p>
     </main>
     )
