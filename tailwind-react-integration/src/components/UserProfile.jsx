8433b7e3674356776aa77264566f4f3d9c4473b7
Athanas Muinde <athanasmwandi5@gmail.com>
2025-09-26 12:44:56 +0300

    fixed error

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile() {
   return (
     <div className=" bg-gray-100 p-8 max-w-sm mx-auto my-20 
-    rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-xs md:max-width-sm
+    rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm
     ">
       <img className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:h-36 md:w-36" src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?w=150&h=150&fit=crop" alt="User" />
       <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">John Doe</h1>
