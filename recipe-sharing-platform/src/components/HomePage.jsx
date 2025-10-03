8c9fea528c4ac747ea11a0f03b2cc73121dcd201
Athanas Muinde <athanasmwandi5@gmail.com>
2025-10-03 14:02:56 +0300

    managed error in fetching data

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -3,17 +3,18 @@ import { useEffect, useState } from "react";
 function Homepage() {
 
     const [recipes, setRecipes] = useState([]);
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         fetch("/data.json")
         .then((res) => res.json())
         .then((data) => setRecipes(data))
-        .catch((err) => console.error("error loading recipes")) 
+        .catch((err) => setError("Failed to load Recipes")) 
     }, [])
 
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
-            {recipes.map((recipe) => (
+            {error ? <p>{error}</p> : recipes.map((recipe) => (
                 <div className="bg-white shadow-md rounded-lg" key={recipe.id}>
                 <img className="w-full h-48 object-cover" src={recipe.image} alt={recipe.title}/>
                 <div className="p-4">
